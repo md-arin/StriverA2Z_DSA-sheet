9170886fdaf7334a94616f26ef543d096bfa6b84
Md Arin Alam <alamlew@gmail.com>
2024-01-30 14:13:26 +0530

    array questions

---
+++ src/Array_questions/Remove_duplicates.java
@@ -0,0 +1,36 @@
+package Array_questions;
+
+import java.util.HashSet;
+
+public class Remove_duplicates {
+    public static void main(String[] args) {
+        int [] nums = {0,0,1,1,1,2,2,3,3,4,5};
+        System.out.println(removeDuplictes(nums));
+    }
+
+   //Optimal sollution BF sollution is using Hashset
+    static int  removeDuplictes(int [] nums){
+        int i = 0;
+        for(int j = 0;j< nums.length; j++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return  i + 1;
+    }
+
+    //Brute force sollution
+    static int removeDuplicates2(int [] nums){
+        HashSet <Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        int k = set.size();
+        int j = 0;
+        for (int x: set) {
+            nums[j++] = x;
+        }
+        return k;
+    }
+}
