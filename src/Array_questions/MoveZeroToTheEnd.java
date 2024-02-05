7179cdc099bb53e2080f867b8e91241c430e13f2
Md Arin Alam <alamlew@gmail.com>
2024-02-05 19:42:06 +0530

    answers added

---
+++ src/Array_questions/MoveZeroToTheEnd.java
@@ -0,0 +1,25 @@
+package Array_questions;
+
+import java.util.Arrays;
+
+public class MoveZeroToTheEnd {
+    public static void main(String[] args) {
+        int [] arr = {4,0,3,2,5};
+        int n = arr.length;
+        sollution(arr,n);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sollution(int []arr, int n){
+      int count = 0;
+      for(int i =0; i< arr.length; i++){
+          if(arr[i] != 0){
+              arr[count++] = arr[i];
+          }
+      }
+
+      while (count<arr.length){
+          arr[count++] = 0;
+      }
+    }
+}
