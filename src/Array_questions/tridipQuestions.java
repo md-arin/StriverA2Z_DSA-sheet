b3a1f50e3b8d2ed63b384523d44d58a8c9298e2a
Md Arin Alam <alamlew@gmail.com>
2024-02-17 13:35:08 +0530

    bs-easy

---
+++ src/Array_questions/tridipQuestions.java
@@ -0,0 +1,16 @@
+package Array_questions;
+
+public class tridipQuestions {
+    public static void main(String[] args) {
+        int []arr = {10,100,10,5};
+        float n = 9.1f;
+    }
+
+    static int sollution(int[]arr, int n){
+        int ans = 0;
+       if(n > arr[0]){
+           n = n -arr[0];
+       }
+       return ans;
+    }
+}
---
+++ src/Binary_search/RBSwithDuplicates.java
@@ -0,0 +1,40 @@
+package Binary_search;
+
+public class RBSwithDuplicates {
+    public static void main(String[] args) {
+        int []arr = {2,5,6,0,0,1,2};
+        System.out.println(solution(arr,3));
+    }
+
+    static boolean solution(int []nums, int target){
+        int s =0;
+        int e = nums.length -1;
+        while (s <= e){
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[s] == nums[mid] && nums[mid] == nums[e]){
+                s = s + 1;
+                e = e -1;
+                continue;
+            }
+
+            if(nums[mid]>=nums[s]){
+                if(nums[s] <= target && target <= nums[mid]){
+                    e = mid - 1;
+                } else{
+                    s = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[e]){
+                    s = mid + 1;
+                } else{
+                    e = mid -1;
+                }
+            }
+        }
+        return false;
+    }
+}
